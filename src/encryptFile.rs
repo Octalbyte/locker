6f61b9e770329571f4b6040028e0de9281a2e2ad
JPSO <jpso.guerreiro@gmail.com>
2022-01-16 20:02:08 +0000

    >>>>>>

--- src/encryptFile.rs
+++ src/encryptFile.rs
@@ -1,25 +1,40 @@
 pub mod hello {
 
+    pub fn hello() {
+        extern crate rand;
+        use rand::rngs::OsRng;
+        use rsa::{PaddingScheme, PublicKey, RsaPrivateKey, RsaPublicKey};
+        use std::str;
 
-pub fn hello(){
-extern crate rand;
-use rsa::{PublicKey, RsaPrivateKey, RsaPublicKey, PaddingScheme};
-use rand::rngs::{OsRng};
+        let mut rng = OsRng;
+        let bits = 2048;
+        let priv_key = RsaPrivateKey::new(&mut rng, bits).expect("failed to generate a key");
+        let pub_key = RsaPublicKey::from(&priv_key);
 
-let mut rng = OsRng;
-let bits = 2048;
-let priv_key = RsaPrivateKey::new(&mut rng, bits).expect("failed to generate a key");
-let pub_key = RsaPublicKey::from(&priv_key);
+        // Encrypt
+        let data = b"hello world";
+        let enc_data = pub_key
+            .encrypt(&mut rng, PaddingScheme::new_pkcs1v15_encrypt(), &data[..])
+            .expect("failed to encrypt");
+        assert_ne!(&data[..], &enc_data[..]);
 
-// Encrypt
-let data = b"hello world";
-let enc_data = pub_key.encrypt(&mut rng, PaddingScheme::new_pkcs1v15_encrypt(), &data[..]).expect("failed to encrypt");
-assert_ne!(&data[..], &enc_data[..]);
+        // Decrypt
+        let dec_data = priv_key
+            .decrypt(PaddingScheme::new_pkcs1v15_encrypt(), &enc_data)
+            .expect("failed to decrypt");
+        assert_eq!(&data[..], &dec_data[..]);
 
-// Decrypt
-let dec_data = priv_key.decrypt(PaddingScheme::new_pkcs1v15_encrypt(), &enc_data).expect("failed to decrypt");
-assert_eq!(&data[..], &dec_data[..]);
+        println!("{:?} -> {:?}", enc_data, dec_data);
 
-println!("{:?} -> {:?}", enc_data, dec_data);
-}
+        let s = match str::from_utf8(&enc_data) {
+            Ok(v) => v,
+            Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
+        };
+        let t = match str::from_utf8(&dec_data) {
+            Ok(v) => v,
+            Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
+        };
+
+        println!("result: {} -> {}", s, t);
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,6 @@ pub mod encryptFile;
 const AUTHOR: &str = "@Octalbyte";
 const VERSION: &str = "0.1.0";
 
-
 fn main() {
     println!("Locker v{} made by {}", VERSION, AUTHOR);
     encryptFile::hello::hello();
