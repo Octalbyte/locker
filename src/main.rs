6c5c10644edbf189dc7712b30ff795703f68ab50
Octalbyte <octalbyte0o52@protonmail.com>
2022-01-16 16:50:06 -0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
+pub mod encryptFile;
+
 const AUTHOR: &str = "@Octalbyte";
 const VERSION: &str = "0.1.0";
 
 
 fn main() {
     println!("Locker v{} made by {}", VERSION, AUTHOR);
-
+    encryptFile::hello::hello();
 }
