0ffa2bd1bc28e9023f17abfe0a186d793ff533e4
JPSO <jpso.guerreiro@gmail.com>
2022-01-17 17:55:19 +0000

    add aes-gmc-siv+format! 🎉✨

--- src/symmetric.rs
+++ src/symmetric.rs
@@ -1,20 +1,21 @@
 mod bytes {
-pub fn encrypt() {
-use aes_gcm_siv::{Aes256GcmSiv, Key, Nonce}; // Or `Aes128GcmSiv`
-use aes_gcm_siv::aead::{Aead, NewAead};
+    pub fn encrypt() {
+        use aes_gcm_siv::aead::{Aead, NewAead};
+        use aes_gcm_siv::{Aes256GcmSiv, Key, Nonce}; // Or `Aes128GcmSiv`
 
-let key = Key::from_slice(b"an example very very secret key.");
-let cipher = Aes256GcmSiv::new(key);
+        let key = Key::from_slice(b"an example very very secret key.");
+        let cipher = Aes256GcmSiv::new(key);
 
-let nonce = Nonce::from_slice(b"unique nonce"); // 96-bits; unique per message
+        let nonce = Nonce::from_slice(b"unique nonce"); // 96-bits; unique per message
 
-let ciphertext = cipher.encrypt(nonce, b"plaintext message".as_ref())
-    .expect("encryption failure!");  // NOTE: handle this error to avoid panics!
+        let ciphertext = cipher
+            .encrypt(nonce, b"plaintext message".as_ref())
+            .expect("encryption failure!"); // NOTE: handle this error to avoid panics!
 
+        let plaintext = cipher
+            .decrypt(nonce, ciphertext.as_ref())
+            .expect("decryption failure!"); // NOTE: handle this error to avoid panics!
 
-let plaintext = cipher.decrypt(nonce, ciphertext.as_ref())
-    .expect("decryption failure!");  // NOTE: handle this error to avoid panics!
-
-assert_eq!(&plaintext, b"plaintext message");
-}
+        assert_eq!(&plaintext, b"plaintext message");
+    }
 }
