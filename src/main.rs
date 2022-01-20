0c8ea0c109cb4003fd4e845adf92530f07dba4f4
JPSO <jpso.guerreiro@gmail.com>
2022-01-20 14:27:32 +0000

    argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use std::env;
+use colored::*;
+use std;
+
 pub mod encrypt_file;
 pub mod symmetric;
 
@@ -6,5 +10,17 @@ const VERSION: &str = "0.1.0";
 
 fn main() {
     println!("Locker v{} made by {}", VERSION, AUTHOR);
-    encrypt_file::encrypt_file::hello();
+    let args: Vec<String> = env::args().collect();
+    let command = &args[1];
+    match command.as_str() {
+        "encrypt" => {
+           
+        }
+        _ => {
+            println!("Command not found: {}", command.bold());
+            println!("Run `locker --help` to get help");
+            std::process::exit(1);
+        }
+    }
+    //encrypt_file::encrypt_file::hello();
 }
