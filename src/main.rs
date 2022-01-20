2b44ca903ff64f26942bc6359fd42e232015f013
JPSO <jpso.guerreiro@gmail.com>
2022-01-20 14:47:18 +0000

    Fixes

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,20 @@
 use std::env;
 use colored::*;
 use std;
+use clap::Parser;
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// Name of the person to greet
+    #[clap(short, long, default_value="")]
+    file: String,
+
+    /// Number of times to greet
+    #[clap(short, long, default_value = "")]
+    string: String,
+}
 
 pub mod encrypt_file;
 pub mod symmetric;
@@ -11,10 +25,15 @@ const VERSION: &str = "0.1.0";
 fn main() {
     println!("Locker v{} made by {}", VERSION, AUTHOR);
     let args: Vec<String> = env::args().collect();
+    let clap_args = Args::parse();
+    if 1 == args.len(){
+        println!("At least one subcommand required");
+        std::process::exit(1);
+    }
     let command = &args[1];
     match command.as_str() {
         "encrypt" => {
-           
+
         }
         _ => {
             println!("Command not found: {}", command.bold());
