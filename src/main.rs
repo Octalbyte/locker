6b040e417b4c535a67303d8e6930b549290d7874
jp <jpso.guerreiro@gmail.com>
2022-01-25 09:36:52 -0300

    Remove useless deps; project restructure

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,6 @@
-extern crate clap;
-
 use std::env;
 use colored::*;
 use std;
-use clap::{
-    App,
-    SubCommand,
-    Arg
-};
 
 pub mod encrypt_file;
 pub mod symmetric;
@@ -17,18 +10,7 @@ const VERSION: &str = "0.1.0";
 
 fn main() {
     println!("Locker v{} made by {}", VERSION, AUTHOR);
-    let clap_args = App::new("locker")
-        .subcommand(SubCommand::with_name("info")
-            .arg(Arg::with_name("verbose")
-                .short("v")
-                .long("verbose"))
-            .arg(Arg::with_name("PARAM")))
-        .subcommand(SubCommand::with_name("sync")
-            .arg(Arg::with_name("encrypt")
-                .short("e")
-                .long("encrypt")))
-        //.default_subcommand("info")    // new method
-        .get_matches();
+   
 
 
     //encrypt_file::encrypt_file::hello();
