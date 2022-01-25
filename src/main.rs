5f78294a21eca74e656a564fd762d4016e1ba0ed
jp <jpso.guerreiro@gmail.com>
2022-01-25 12:26:50 -0300

    Stuff

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::env;
 use colored::*;
 use std;
-
+use minparse::minparse;
 pub mod encrypt_file;
 pub mod symmetric;
 
@@ -11,7 +11,25 @@ const VERSION: &str = "0.1.0";
 fn main() {
     println!("Locker v{} made by {}", VERSION, AUTHOR);
    
+    let subc = minparse::subcommands();
+    match subc[1] {
+        "encrypt" => {
+            match subc[2] {
+                "file" => {
 
+                }
+
+            }
+        },
+        "help" => {
+            match subc[2] {
+                "encrypt" => {}
+                "decrypt" => {}
+                "hash" => {}
+                "check" => {}
+            }
+        }
+    }
 
     //encrypt_file::encrypt_file::hello();
 }
