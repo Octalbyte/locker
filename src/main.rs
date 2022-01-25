6ea4f702dad898091693fd5718ef45d809ae1b2f
jp <jpso.guerreiro@gmail.com>
2022-01-25 12:34:18 -0300

    fixes

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,40 @@
-use std::env;
 use colored::*;
-use std;
 use minparse::minparse;
 pub mod encrypt_file;
 pub mod symmetric;
-
+use std;
 const AUTHOR: &str = "@Octalbyte";
 const VERSION: &str = "0.1.0";
 
 fn main() {
-    println!("Locker v{} made by {}", VERSION, AUTHOR);
+    println!("Locker v{} made by {}", VERSION.bold(), AUTHOR.bold());
    
     let subc = minparse::subcommands();
-    match subc[1] {
+    if subc.len() == 1 {
+        println!("");
+        std::process::exit(0);
+    }
+    match subc[1].as_str() {
         "encrypt" => {
-            match subc[2] {
+            match subc[2].as_str() {
                 "file" => {
 
                 }
+                _ => {}
 
             }
         },
         "help" => {
-            match subc[2] {
+            match subc[2].as_str() {
                 "encrypt" => {}
                 "decrypt" => {}
                 "hash" => {}
                 "check" => {}
+                _ => {}
             }
-        }
+        },
+
+        _ => {}
     }
 
     //encrypt_file::encrypt_file::hello();
